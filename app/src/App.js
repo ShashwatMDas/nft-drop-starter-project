1b57725b479daeb185c42eeb0cae19949a021f34
ShashwatMDas <shashwatmdas@gmail.com>
2022-04-22 21:41:49 +0530

    Created nfts config and webapp connection

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,59 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
+import CandyMachine from "./CandyMachine";
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+	const [walletAddress, setWalletAddress] = useState(null);
+	const checkIfWalletIsConnected = async () => {
+		try {
+			const { solana } = window;
+			if(solana && solana.isPhantom) {
+				console.log('Wallet is there');
+				const phantom = await solana.connect({
+					onlyIfTrusted: true
+				});
+				console.log('Connected to wallet with:', phantom.publicKey.toString());
+				setWalletAddress(phantom.publicKey.toString());
+			} else {
+				alert("Solana wallet not found");
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}
+	const connectWallet = async () => {
+		const { solana } = window;
+		if(solana) {
+			const phantom = await solana.connect();
+			console.log('Connected to wallet with:', phantom.publicKey.toString());
+			setWalletAddress(phantom.publicKey.toString());
+		}
+	}
+	useEffect(() => {
+		const onLoad = async () => {
+			await checkIfWalletIsConnected();
+		}
+		window.addEventListener('load', onLoad);
+		return () => {
+			window.removeEventListener('load', onLoad);
+		}
+	}, [])
 	return (
 		<div className="App">
 			<div className="container">
 				<div className="header-container">
 					<p className="header">🍭 Candy Drop</p>
 					<p className="sub-text">NFT drop machine with fair mint</p>
+					{!walletAddress && <button className='cta-button connect-wallet-button' onClick={connectWallet}>Connect Wallet</button>}
 				</div>
+				{
+					walletAddress && <CandyMachine walletAddress={window.solana} />
+				}
 				<div className="footer-container">
 					<img
 						alt="Twitter Logo"
--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -20,6 +20,60 @@ const opts = {
 };
 
 const CandyMachine = ({ walletAddress }) => {
+  const [candyMachine, setCandyMachine] = React.useState(null);
+  React.useEffect(() => {
+    getCandyMachineSate();
+  }, [])
+
+  const getProvider = () => {
+    const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST;
+    const connection = new Connection(rpcHost);
+    const provider = new Provider(connection, window.solana, opts.preflightCommitment);
+    return provider;
+  }
+
+  const getCandyMachineSate = async () => {
+    const provider = getProvider();
+    const idl = await Program.fetchIdl(candyMachineProgram, provider);
+    const program = new Program(idl, candyMachineProgram, provider);
+    const candyMachine = await program.account.candyMachine.fetch(process.env.REACT_APP_CANDY_MACHINE_ID);
+    const itemsAvailable = candyMachine.data.itemsAvailable.toNumber();
+    const itemsRedeemed = candyMachine.itemsRedeemed.toNumber();
+    const itemsRemaining = itemsAvailable - itemsRedeemed;
+    const goLiveDate = candyMachine.data.goLiveDate.toNumber();
+    const goLiveDateTimeString = `${new Date(goLiveDate*1000).toGMTString()}`;
+    const presale = candyMachine.data.whitelistMintSettings && candyMachine.data.whitelistMintSettings.presale && (!candyMachine.data.goLiveDate || candyMachine.data.goLiveDate.toNumber() > new Date().getTime() / 1000);
+    setCandyMachine({
+        id: process.env.REACT_APP_CANDY_MACHINE_ID,
+        program,
+        state: {
+            itemsAvailable,
+            itemsRedeemed,
+            itemsRemaining,
+            goLiveDate,
+            goLiveDateTimeString,
+            isSoldOut: itemsRemaining === 0,
+            isActive:
+                (presale ||
+                    candyMachine.data.goLiveDate.toNumber() < new Date().getTime() / 1000) &&
+                (candyMachine.endSettings
+                    ? candyMachine.endSettings.endSettingType.date
+                        ? candyMachine.endSettings.number.toNumber() > new Date().getTime() / 1000
+                        : itemsRedeemed < candyMachine.endSettings.number.toNumber()
+                    : true),
+            isPresale: presale,
+            goLiveDate: candyMachine.data.goLiveDate,
+            treasury: candyMachine.wallet,
+            tokenMint: candyMachine.tokenMint,
+            gatekeeper: candyMachine.data.gatekeeper,
+            endSettings: candyMachine.data.endSettings,
+            whitelistMintSettings: candyMachine.data.whitelistMintSettings,
+            hiddenSettings: candyMachine.data.hiddenSettings,
+            price: candyMachine.data.price,
+        },
+    });
+    console.log(itemsAvailable, itemsRedeemed, goLiveDate, goLiveDateTimeString);
+  }
 
   const getCandyMachineCreator = async (candyMachine) => {
     const candyMachineID = new PublicKey(candyMachine);
@@ -299,8 +353,8 @@ const CandyMachine = ({ walletAddress }) => {
 
   return (
     <div className="machine-container">
-      <p>Drop Date:</p>
-      <p>Items Minted:</p>
+      <p>Drop Date: {candyMachine?.state?.goLiveDateTimeString}</p>
+      <p>Items Minted: {candyMachine?.state?.itemsRedeemed} / {candyMachine?.state?.itemsAvailable}</p>
       <button className="cta-button mint-button" onClick={mintToken}>
         Mint NFT
       </button>
